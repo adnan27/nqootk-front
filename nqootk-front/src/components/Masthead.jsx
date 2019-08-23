e3be4351f3763820cd52b9fdc842b766d2d6d6ec
m-dxj <m-dxj@DESKTOP-M2EED5J>
2019-08-23 21:06:09 +0300

    fixed warnings

--- nqootk-front/src/components/Masthead.jsx
+++ nqootk-front/src/components/Masthead.jsx
@@ -4,16 +4,16 @@ import React, {Component} from 'react';
     {	
 	render(){
 		return(
-			<header class="masthead">
-				<div class="container h-100">
-					<div class="row h-100 align-items-center justify-content-center text-center">
-						<div class="col-lg-10 align-self-end">
-							<h1 class="text-uppercase text-white font-weight-bold">Nqootk.com</h1>
-							<hr class="divider my-4"></hr>
+			<header className="masthead">
+				<div className="container h-100">
+					<div className="row h-100 align-items-center justify-content-center text-center">
+						<div className="col-lg-10 align-self-end">
+							<h1 className="text-uppercase text-white font-weight-bold">Nqootk.com</h1>
+							<hr className="divider my-4"></hr>
 						</div>
-						<div class="col-lg-8 align-self-baseline">
-							<p class="text-white-75 font-weight-light mb-5">اهلا وسهلا بكم في موقع نقوطك, الذي بدوره يسهل على المستخدمين تخزين اسماء المناسبات او الاعراس التي قاموا بحضورها بالاضافة الى تسجيل مبلغ النقوط لكل مناسبة.</p>
-							<a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">تسدجيل الدخول</a>
+						<div className="col-lg-8 align-self-baseline">
+							<p className="text-white-75 font-weight-light mb-5">اهلا وسهلا بكم في موقع نقوطك, الذي بدوره يسهل على المستخدمين تخزين اسماء المناسبات او الاعراس التي قاموا بحضورها بالاضافة الى تسجيل مبلغ النقوط لكل مناسبة.</p>
+							<a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">تسدجيل الدخول</a>
 						</div>
 					</div>
 				</div>
--- nqootk-front/src/index.js
+++ nqootk-front/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from  './components/App';
-ReactDOM.render(<App />,document.getElementById('root'));
+import HomePage from './pages/HomePage';
+ReactDOM.render(<HomePage />,document.getElementById('root'));
